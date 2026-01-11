0d8dbc4ecfe7c4ef4d6117269da99e8d5ea4989a
jw <wjd10577@naver.com>
2026-01-11 16:50:29 +0900

    first commit

---
+++ src/main/java/com/example/apigatewayservice/JwtAuthenticationFilter.java
@@ -0,0 +1,56 @@
+package com.example.apigatewayservice;
+
+import java.nio.charset.StandardCharsets;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<Object> {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Override
+    public GatewayFilter apply(Object config) {
+        return (exchange, chain) -> {
+            String token = exchange.getRequest()
+                .getHeaders()
+                .getFirst("Authentication");
+            System.out.println("토큰 "+ token);
+
+            if (token == null) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+
+            SecretKey secretKey = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+            String subject = Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+            System.out.println("userId " + subject);
+
+            return chain.filter(
+                exchange.mutate()
+                    .request(
+                        exchange.getRequest()
+                            .mutate()
+                            .header("X-User-Id", subject)
+                            .build()
+                    )
+                    .build()
+            );
+        };
+    }
+}
